d66da7dcfd31f8ab3b03149b983486869733ef75
RamosFonseca <beidseitig@gmail.com>
2022-02-17 14:58:07 -0300

    teste

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/Component.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/Component.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
-function me () {
-    return (
-        <div>
-            <h1>Ramos Fonseca</h1>
-            <p>Eu gosto de café, música, livros e games!!!</p>
-        </div>
-    );
+class HelloWorld extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Ramos Fonseca</h1>
+                <p>Eu gosto de café, música, livros e games!!!</p>
+            </div>
+        );
+    }
 }
 
-export default me;
+export default Component;
