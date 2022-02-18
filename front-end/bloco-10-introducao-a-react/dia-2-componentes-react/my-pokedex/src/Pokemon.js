8db8cab77c10d3ee20db936657da6505800217f8
RamosFonseca <beidseitig@gmail.com>
2022-02-18 13:34:41 -0300

    Exercicios do dia 2 do bloco 10 de front end, introduçao ao react

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Pokemon extends React.Component {
+    render() {
+        const { pokemon: { name, type, averageWeight, image } } = this.props;
+
+        return (
+            <div className='pokemon'>
+                <div>
+                    <p>{ name }</p>
+                    <p>{ type }</p>
+                    <p>
+                        {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}
+                    </p>
+                </div>
+                <img src={ image } alt={ `${name} picture` } />
+            </div>
+        );
+    }
+}
+
+export default Pokemon;
