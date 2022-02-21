2cda44778ed5b0088aeda7eb97e3261491e3b2ef
RamosFonseca <beidseitig@gmail.com>
2022-02-21 14:33:22 -0300

    Exercicio/10.2

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
-import Image from './Image';
+import React from "react";
+import "./App.css";
+import Image from "./Image";
 
 class App extends React.Component {
   render() {
-    return(
-      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
-    )
+    return (
+      <Image
+        source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+        alternativeText="Cute cat staring"
+      />
+    );
   }
 }
 
